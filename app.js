629b3ea71f139e24b6f61abc16f23df035de7326
Lashaun <clarklas831@gmail.com>
2021-08-07 11:06:15 -0500

    done

--- app.js
+++ app.js
@@ -52,7 +52,6 @@ Add a New Project
 =================
 `);
 
-  // If there's no 'projects' array property, create one
   if (!portfolioData.projects) {
     portfolioData.projects = [];
   }
@@ -130,10 +129,10 @@ promptUser()
   .then(promptProject)
   .then((portfolioData) => {
     console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, (err) => {
+      if (err) throw new Error(err);
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
